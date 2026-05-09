d1f33d4f38718505fdde9d1e289f39576858d509
lzhsus <1434253600@qq.com>
2026-05-09 15:54:41 +0800

    add ts pet manager

---
+++ assets/scripts/manager/PetManager.ts
@@ -0,0 +1,52 @@
+import { Http } from '../core/Http';
+import { PetModel } from '../model/PetModel';
+import { UserManager } from './UserManager';
+
+export class PetManager {
+  static pet: PetModel | null = null;
+
+  static async getInfo(): Promise<PetModel> {
+    const res = await Http.get<{
+      pet: PetModel;
+    }>('/api/pet/info');
+
+    if (res.code !== 0) {
+      throw new Error(res.message || '获取宠物失败');
+    }
+
+    this.pet = res.data.pet;
+
+    return this.pet;
+  }
+
+  static async feed(): Promise<PetModel> {
+    return this.action('/api/pet/feed');
+  }
+
+  static async bath(): Promise<PetModel> {
+    return this.action('/api/pet/bath');
+  }
+
+  static async play(): Promise<PetModel> {
+    return this.action('/api/pet/play');
+  }
+
+  private static async action(url: string): Promise<PetModel> {
+    const res = await Http.post<{
+      user: any;
+      pet: PetModel;
+    }>(url);
+
+    if (res.code !== 0) {
+      throw new Error(res.message || '宠物操作失败');
+    }
+
+    this.pet = res.data.pet;
+
+    if (res.data.user) {
+      UserManager.updateUser(res.data.user);
+    }
+
+    return this.pet;
+  }
+}
